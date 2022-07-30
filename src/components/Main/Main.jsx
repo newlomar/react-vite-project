b389815e328e22d13476f3767ea0e0749329d222
Newton Lomar <newtonlomar16@gmail.com>
2022-07-30 15:22:28 -0300

    finished navbar style adjusts and started main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main content goes here</h1>;
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was Originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
