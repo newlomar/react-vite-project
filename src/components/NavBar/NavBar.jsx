434c671c0584b275feb4395f62955b856e511810
Newton Lomar <newtonlomar16@gmail.com>
2022-07-30 13:53:39 -0300

    navbar completed design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,13 @@
+import logo from '../../assets/reactjs-icon.svg';
+
 export default function NavBar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <header>
+      <nav className='nav'>
+        <img className='logo' src={logo} alt='React Library Logo' />
+        <h3 className='h3'>ReactFacts</h3>
+        <h4 className='h4'>React Course - Project 1</h4>
+      </nav>
+    </header>
+  );
 }
