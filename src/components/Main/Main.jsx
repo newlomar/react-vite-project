3c41e8d6f666dcc2ee3693090a849bd57953a6ae
Newton Lomar <newtonlomar16@gmail.com>
2022-07-30 15:37:33 -0300

    developing the main content

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,15 @@
 export default function Main() {
   return (
-    <main>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was Originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    <main class='container'>
+      <h1 className='h1'>Fun facts about React</h1>
+      <ul className='list'>
+        <li className='list-item'>Was first released in 2013</li>
+        <li className='list-item'>Was Originally created by Jordan Walke</li>
+        <li className='list-item'>Has well over 100K stars on Github</li>
+        <li className='list-item'>Is maintained by Facebook</li>
+        <li className='list-item'>
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
       </ul>
     </main>
   );
